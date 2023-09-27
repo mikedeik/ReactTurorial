f4b678235e765035b160e674f42c772c7cf4d929
xitdeveloper <info@xit.gr>
2023-09-27 16:38:26 +0300

    first task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const getRequest = async () => {
+    try {
+      const response = await axios.get("https://api.publicapis.org/entries", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setData(response.data.entries);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  // First Task
+  // Na emfanisoume se ul -> li ola ta descriptions apo ta data pou exoume parei apo ta api
+  // Molis to kaneis dokimase css na pane ola terma aristera
+  // kai i lista na pianei to 80% tis othonis
+
+  useEffect(() => {
+    getRequest();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Tutorial</h1>
+      {data.length > 0 && <div>{data[0].Description}</div>}
     </div>
   );
 }
